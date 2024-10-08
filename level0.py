94888248159fc8cf3f2b915aea360f6dcc0b484f
Tfregues <rafaelitoto895@gmail.com>
2024-10-08 02:14:46 -0300

    print para log

--- level0.py
+++ level0.py
@@ -46,7 +46,7 @@ def detectar_aspecto_na_tela(imagem_modelo, threshold=0.8):    # Detecta a prese
 
     modelo = cv2.imread(imagem_modelo, cv2.IMREAD_COLOR)
     if modelo is None:
-        print(f"Erro: a imagem modelo não pôde ser carregada de {imagem_modelo}. Verifique o caminho.")
+        log(f"Erro: a imagem modelo não pôde ser carregada de {imagem_modelo}. Verifique o caminho.")
         return None, None
 
     if len(modelo.shape) == 3 and modelo.shape[2] == 4:
@@ -66,7 +66,7 @@ def encontrar_imagem_e_clicar(imagem, descricao):    # Encontra uma imagem na te
         centro_y = posicao[1] + tamanho[0] // 2  # Centro vertical
         pyautogui.moveTo(centro_x, centro_y)
         pyautogui.click()
-        print(f"'{descricao}' encontrada e clicada.")
+        log(f"'{descricao}' encontrada e clicada.")
         return True
     return False
 
@@ -77,24 +77,25 @@ def encontrar_imagem_e_clicar_abaixo(imagem, descricao):    # Encontra uma image
         centro_y = posicao[1] + tamanho[0] + 1  # Um pouco abaixo da imagem (1 pixels)
         pyautogui.moveTo(centro_x, centro_y)
         pyautogui.click()
-        print(f"'{descricao}' encontrada e clicada abaixo da imagem.")
+        log(f"'{descricao}' encontrada e clicada abaixo da imagem.")
         return True
     return False
 
 def verificar_reconectar(imagem_condicao, descricao):    # Verifica a condição de reconexão e clica se a imagem for encontrada.
+#    imagem_condicao = r'data\server_down.png'
     posicao, tamanho = detectar_aspecto_na_tela(imagem_condicao)
     if posicao:
         centro_x = posicao[0] + tamanho[1] // 2
         centro_y = posicao[1] + tamanho[0] // 2
         pyautogui.moveTo(centro_x, centro_y)
         pyautogui.click()
-        print(f"'{descricao}' encontrada e clicada. Reiniciando o processo.")
+        log(f"'{descricao}' encontrada e clicada. Reiniciando o processo.")
         return True
     return False
 
 def macro_leaving_mission():    # Executa a macro para deixar a missão.
-    print("Executando macro LEAVING MISSION...")
+    log("Executando macro LEAVING MISSION...")
     for _ in range(4):
         pyautogui.press('esc')
         time.sleep(1.5)
-    print("Macro LEAVING MISSION concluída.")
+    log("Macro LEAVING MISSION concluída.")
