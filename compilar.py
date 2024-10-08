1fab923cf2345009f3ea1108085a8b92bf51b287
Tfregues <rafaelitoto895@gmail.com>
2024-10-08 02:27:35 -0300

    alguns ajustes feito e codigo "completo"
    Não testado

--- compilar.py
+++ compilar.py
@@ -3,24 +3,19 @@ import shutil
 import subprocess
 
 def compilar():
-    # Nome do script principal
     script_principal = 'main.py'
     arquivo_icon = 'icon.png'
 
-    # Criação do executável usando PyInstaller
     subprocess.run(['pyinstaller', '--onefile', '--icon=' + arquivo_icon, script_principal])
 
-    # Copia o arquivo icon.ico para a pasta dist
     pasta_dist = 'dist'
 
-    # Verifica se a pasta dist existe
     if os.path.exists(pasta_dist):
         shutil.copy(arquivo_icon, os.path.join(pasta_dist, arquivo_icon))
         print(f'{arquivo_icon} copiado para {pasta_dist}/')
     else:
         print('A pasta dist não foi encontrada.')
     
-    # Mover a pasta 'data' para o diretório de saída (onde o executável foi gerado)
     if os.path.exists('data'):
         shutil.copytree('data', os.path.join(pasta_dist, 'data'))
         print("Pasta 'data' movida com sucesso!")
--- level4.py
+++ level4.py
@@ -6,7 +6,7 @@ import level0
 def run_level4():
     global parar
     level0.parar = 0 False
-    contador = 0
+    contador = 1
 
     if level0.verificar_interrupcao():
         return
@@ -27,7 +27,7 @@ def run_level4():
         level0.log(f"Imagem 1 não encontrada.\nTentando novamente...({contador})", level='INFO')
         contador += 1
 
-    contador = 0
+    contador = 1
 
     # Encontrar Imagem 2
     while not level0.encontrar_imagem_e_clicar(r'data\level\level4\r2.png', 'Imagem 2'):
@@ -41,7 +41,7 @@ def run_level4():
         level0.log(f"Imagem 2 não encontrada.\nTentando novamente...({contador})", level='INFO')
         contador += 1
 
-    contador = 0
+    contador = 1
 
     # Encontrar Imagem 3
     while not level0.encontrar_imagem_e_clicar(r'data\level\level4\r3.png', 'Imagem 3'):
@@ -55,7 +55,7 @@ def run_level4():
         level0.log(f"Imagem 3 não encontrada.\nTentando novamente...({contador})", level='INFO')
         contador += 1
 
-    contador = 0
+    contador = 1
 
     # Encontrar Imagem 4 ou 4v2
     while not level0.encontrar_imagem_e_clicar(r'data\level\level4\r4.png', 'Imagem 4'):
@@ -72,7 +72,7 @@ def run_level4():
         level0.log(f"Imagem 4 não encontrada.\nTentando novamente...({contador})", level='INFO')
         contador += 1
 
-    contador = 0
+    contador = 1
 
     # Encontrar Imagem 5
     while True:
@@ -81,5 +81,44 @@ def run_level4():
             continue
         level0.log("Procurando por Imagem 5a..." level='INFO')
         while not level0.encontrar_imagem_e_clicar(r'data\level\level4\r5a', 'Imagem 5a'):
-            if level0.verificar_reconectar
+            if level0.verificar_reconectar(r'data\server_down.png', 'Server Down'):
+                level0.log("'Server Down' encontrada.\nReniciando o ciclo do início.", level='WARNING')
+                return True
+            if level0.verificar_interrupcao():
+                return
+            if level0.parar:
+                return
+            level0.log(f"Imagem 5a não encontrada.\nTentando novamente...({contador})", level='INFO')
+            contador += 1
 
+        contador = 1
+
+        level0.log("Procurando por Imagem 5b...", level='INFO')
+        encontrada_5b = False
+        for _ in range(3):
+            if level0.verificar_reconectar(r'data\server_down.png', 'Server Down'):
+                level0.log("'Server Down' encontrada.\nReiniciando o ciclo do início.", level='WARNING')
+                continue
+            if level0.verificar_interrupcao():
+                return
+            if level0.parar:
+                return
+            if level0.encontrar_imagem_e_clicar(r'data\level\level4\r5b.png', 'Imagem 5b'):
+                level0.log("Imagem 5b encontrada.\nClicando ESC uma vez.", level='INFO')
+                pyautogui.press('esc')
+                encontrada_5b = True
+                break
+
+        if not encontrada_5b:
+            level0.log("Imagem 5b não encontrada.\nRetornando à busca pela Imagem 5a.", level='INFO')
+        else:
+            break
+        
+
+
+if __name__ == "__name__":
+    try:
+        run_level4()
+    finally:
+        level0.log_file.close()
+        
